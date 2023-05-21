0202238a0a15498e844fcdf51a2a6531b4d00ce0
Wang Shenghe <h469hr@inf.elte.hu>
2023-05-21 18:16:21 +0200

    Update the readme

--- approach_detection.py
+++ approach_detection.py
@@ -8,7 +8,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument('-n', '--network', default="normal", choices=["normal", "tiny", "prn", "v4-tiny"],
                 help='Network Type')
 ap.add_argument('-d', '--device', type=int, default=0, help='Device to use')
-ap.add_argument('-s', '--size', default=416, help='Size for yolo')
+ap.add_argument('-s', '--size', default=256, help='Size for yolo')
 ap.add_argument('-c', '--confidence', default=0.2, help='Confidence for yolo')
 ap.add_argument('-nh', '--hands', default=-1, help='Total number of hands to be detected per frame (-1 for all)')
 args = ap.parse_args()
@@ -67,8 +67,6 @@ while rval:
     # display hands
     for detection in results[:hand_count]:
         id, name, confidence, x, y, w, h = detection
-        cx = x + (w / 2)
-        cy = y + (h / 2)
         records.append(w * h)
         if len(records) != 1:
             if records[-1] > records[-2]:
